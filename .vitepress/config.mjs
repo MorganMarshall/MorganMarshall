0eb4603b9f397a935f5352f4dc0e560cb4a2d4b1
Morgan Marshall <imorganmarshall@gmail.com>
2025-09-22 10:32:59 -0400

    remove local search provider, use default search configuration

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -83,9 +83,7 @@ export default defineConfig({
   ],
 
   themeConfig: {
-    search: {
-      provider: "local",
-    },
+
     // https://vitepress.dev/reference/default-theme-config
     // logo: { src: "/logo.svg", alt: "Morgan Marshall Logo" },
     nav: [{ text: "Home", link: "/" }],
