7c1955a499dce3fe01c23f6107b9eea4907ebb1e
Morgan Marshall <imorganmarshall@gmail.com>
2026-07-01 02:51:12 -0400

    add google tag

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,22 @@
 import DefaultTheme from "vitepress/theme";
+import { h } from "vue";
 //import BlogIndex from "./theme/BlogIndex.vue";
 import "./custom.css";
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      // Injects the GTM noscript iframe at the very top of the <body> wrapper
+      "layout-top": () =>
+        h("noscript", {}, [
+          h("iframe", {
+            src: "https://www.googletagmanager.com/ns.html?id=GTM-5LB44MBK",
+            height: "0",
+            width: "0",
+            style: "display:none;visibility:hidden",
+          }),
+        ]),
+    });
+  },
+};
